53f174abcb43ef447d59af6fac426b6f0a7a8f06
Paula Jimenez <pjimvel@gmail.com>
2017-03-29 23:02:27 +0200

    done

--- rover.js
+++ rover.js
@@ -3,6 +3,19 @@ var myRover = {
   direction: 'N'
 };
 
+var obstacles = {
+  position1: [5,5],
+  position2: [2,2],
+  position3: [3,8],
+  position4: [8,3]
+};
+
+var nextPosition = [];   //Variable auxiliar para la detección de obstáculos
+nextPosition[0] = myRover.position[0];
+nextPosition[1] = myRover.position[1];
+var stop;           //Variable auxiliar para el caso de encontrar obstáculo
+
+//Creación de la malla de 10x10
 var dimension = 10;
 var myGrid = new Array(dimension);
 for (var i=0; i<dimension; i++) {
@@ -12,64 +25,63 @@ for (var i=0; i<dimension; i++) {
   }
 }
 
+//Funciones de movimiento del Rover
 function goForward(rover) {
   switch(rover.direction) {
     case "N":
       if(rover.position[0]===10) {
-        rover.position[0] = 0;
+        nextPosition[0] = 0;
       }
-      rover.position[0]++;
+      nextPosition[0]++;
       break;
     case "E":
       if(rover.position[1]===10) {
-        rover.position[1]=0;
+        nextPosition[1] = 0;
       }
-      rover.position[1]++;
+      nextPosition[1]++;
       break;
     case "S":
       if(rover.position[0]===0) {
-        rover.position[0]=10;
+        nextPosition[0] = 10;
       }
-      rover.position[0]--;
+      nextPosition[0]--;
       break;
     case "W":
       if(rover.position[1]===0) {
-        rover.position[1]=10;
+        nextPosition[1] = 10;
       }
-      rover.position[1]--;
+      nextPosition[1]--;
       break;
   }
-  console.log("New Rover Position: [" + rover.position[0] + "," + rover.position[1] + "]. Rover direction: " + rover.direction);
 }
 
 function goBackward(rover) {
   switch(rover.direction) {
     case "N":
       if(rover.position[0]===0) {
-        rover.position[0]=10;
+        nextPosition[0] = 10;
       }
-      rover.position[0]--;
+      nextPosition[0]--;
       break;
     case "E":
       if(rover.position[1]===0) {
-        rover.position[1]=10;
+        nextPosition[1] = 10;
       }
-      rover.position[1]--;
+      nextPosition[1]--;
       break;
     case "S":
       if(rover.position[0]===10) {
-        rover.position[0]=0;
+        nextPosition[0] = 10;
       }
-      rover.position[0]++;
+      nextPosition[0]++;
       break;
     case "W":
       if (rover.position[1]===10) {
-        rover.position=0;
+        nextPosition[1] = 0;
       }
-      rover.position[1]++;
+      nextPosition[1]++;
       break;
   }
-  console.log("New Rover Position: [" + rover.position[0] + "," + rover.position[1] + "]. Rover direction: " + rover.direction);
 }
 
 function turnRight(rover) {
@@ -87,7 +99,6 @@ function turnRight(rover) {
       rover.direction = "N";
       break;
   }
-  console.log("Rover position: [" + rover.position[0] + "," + rover.position[1] + "]. Rover direction: " + rover.direction);
 }
 
 function turnLeft(rover) {
@@ -105,12 +116,13 @@ function turnLeft(rover) {
       rover.direction = "S";
       break;
   }
-  console.log("Rover position: [" + rover.position[0] + "," + rover.position[1] + "]. Rover direction: " + rover.direction);
 }
 
+//Petición al usuario de los comandos a ejecutar por el rover
 alert("Hello there! \n\nThe Rover in Mars is waiting for your instructions. \nSend him as many commands as you want. \nThe possible commands are: \n\n   - f for go forward \n   - b for go back \n   - r to turn right \n   - l to turn left");
 var instructions = prompt("Commands to the Mars Rover:");
 
+//Movimiento del rover por la malla
 for (var k=0; k<instructions.length; k++) {
   switch (instructions[k]) {
     case "f":
@@ -132,6 +144,22 @@ for (var k=0; k<instructions.length; k++) {
     default:
       break;
   }
+  if (nextPosition[0]===obstacles.position1[0] && nextPosition[1]===obstacles.position1[1]) {stop = 1;}
+  else if (nextPosition[0]===obstacles.position2[0] && nextPosition[1]===obstacles.position2[1]) {stop = 1;}
+  else if (nextPosition[0]===obstacles.position3[0] && nextPosition[1]===obstacles.position3[1]) {stop = 1;}
+  else if (nextPosition[0]===obstacles.position4[0] && nextPosition[1]===obstacles.position4[1]) {stop = 1;}
+
+  if (stop===1) {
+    console.log("Report: Obstacle in position [" + nextPosition[0] + "," + nextPosition[1] + "]");
+    break;
+  }
+  else {
+    myRover.position[0] = nextPosition[0];
+    myRover.position[1] = nextPosition[1];
+  }
+  console.log("Rover position: [" + myRover.position[0] + "," + myRover.position[1] + "]. Rover direction: " + myRover.direction);
 }
+
+//Mensaje final
 console.log("Final Rover Position: [" + myRover.position[0] + "," + myRover.position[1] + "]. Rover direction: " + myRover.direction);
 console.log("Mission accomplished!");
